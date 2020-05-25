25d6d271d5101c234a8bd80e65d4b5e95bb3988f
Surya Prakash Koidala <51915675+Surya291@users.noreply.github.com>
2020-05-25 21:40:57 +0530

    Create the_hurdle_race.cpp

---
+++ Algorithms/Implemetation/the_hurdle_race.cpp
@@ -0,0 +1,13 @@
+vector<string> split_string(string);
+
+// Complete the hurdleRace function below.
+int hurdleRace(int k, vector<int> height) {
+sort(height.begin(),height.end());
+if(k >= height[height.size()-1]){
+    return 0;
+}
+else{
+    return height[height.size()-1] - k;
+}
+
+}
